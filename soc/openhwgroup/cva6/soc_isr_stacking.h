fc61bc3f13a0c129998f5f03dc047c0a7f413983
Matteo Bruno Franco <matteo.brunofranco@edu.unito.it>
2025-03-16 17:36:06 +0100

    moved up the unstacking HW

--- soc/openhwgroup/cva6/soc_isr_stacking.h
+++ soc/openhwgroup/cva6/soc_isr_stacking.h
@@ -42,6 +42,10 @@
 
 #else /* _ASMLANGUAGE */
 
+.macro get_current_ready_q_cache dst
+	la \dst, _kernel + ___kernel_t_ready_q_OFFSET + ___ready_q_t_cache_OFFSET	
+.endm
+
 /*
  * Size of the HW managed part of the ESF:
  *    sizeof unsigned long * 16 saved registers
@@ -77,12 +81,14 @@
 */
 #define SOC_ISR_HW_UNSTACKING			\
 	get_current_ready_q_cache t3;		\
-	lr t2, ___cpu_t_current_OFFSET(s0);	\			
+	lr t3,0(t3);						\
+	lr t2, ___cpu_t_current_OFFSET(s0);	\
+	mv t4, sp;							\
 	beq	t3, t2,	1f;						\
-	lr t3, _thread_offset_to_sp(t3);	\
+	lr t4, _thread_offset_to_sp(t3);	\
 1:										\
-	addi t3,t3,ESF_SW_SIZEOF;			\
-	csrw 0x7C9, t3;		                \
+	addi t4,t4,ESF_SW_SIZEOF;			\
+	csrw 0x7C9, t4;		                \
 	li t0, (0x1 << 26);                 \
 	csrs 0x7C7, t0;
 
