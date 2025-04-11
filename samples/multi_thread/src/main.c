24036a1df8c8e0c94ce63b4ac5b1230e31018fe7
Matteo Bruno Franco <matteo.brunofranco@edu.unito.it>
2025-04-11 14:34:10 +0200

    test ancora non funzionanti

--- samples/multi_thread/src/main.c
+++ samples/multi_thread/src/main.c
@@ -8,30 +8,37 @@
 /* Definizione delle priorità dei thread (valore minore = maggiore priorità) */
 #define THREAD1_PRIORITY 5
 #define THREAD2_PRIORITY 5
+#define FAKE_IDLE_PRIORITY 10
 #define NUM_LOOP 10
 
 /* Definizione delle strutture dati per i thread */
 K_THREAD_STACK_DEFINE(thread1_stack, STACK_SIZE);
 K_THREAD_STACK_DEFINE(thread2_stack, STACK_SIZE);
-struct k_thread thread1_data;
-struct k_thread thread2_data;
+K_THREAD_STACK_DEFINE(idle_stack, STACK_SIZE);
+struct k_thread thread1_data, thread2_data, fake_idle;
 int end_loop = 1, end_t = 1;
 uint64_t mret_start,mret_end;
 
 void my_timer_handler(struct k_timer *dummy)
 {
-	printf("interrupt\n");
+	//printf("interrupt\n");
 	end_loop = 0;
 	ARG_UNUSED(dummy);
 }
 
+/*void fake_idle_thread(void *arg1,void *arg2,void *arg3){
+	ARG_UNUSED(arg1);
+	ARG_UNUSED(arg2);
+	ARG_UNUSED(arg3);
+	while(end_t);
+}*/
 
 K_TIMER_DEFINE(my_timer, my_timer_handler, NULL);
 
 
 void thread1(void *arg1, void *arg2, void *arg3) {
    while(end_t){
-		printf("t1 sleeping\n");
+		//printf("t1 sleeping\n");
 		k_sleep(K_MSEC(50));
 	}
 	printf("t fine\n");
@@ -52,27 +59,33 @@ static inline void enable_pmc(void) {
 }
 
 int main(void) {
-	enable_pmc();
-	__asm__ volatile ("csrw mhpmevent3, 28");
-	
+	k_sleep(K_MSEC(1));
+	//enable_pmc();
+	//__asm__ volatile ("csrw mhpmevent3, 28");
     k_tid_t t1 = k_thread_create(&thread1_data, thread1_stack, STACK_SIZE,
                                    thread1, NULL, NULL, NULL,
                                    THREAD1_PRIORITY, 0, K_NO_WAIT);
+	k_thread_name_set(t1,"T1");
+	/*k_tid_t idle_t = k_thread_create(&fake_idle, idle_stack, STACK_SIZE,
+                                   fake_idle_thread, NULL, NULL, NULL,
+                                   FAKE_IDLE_PRIORITY, 0, K_NO_WAIT);	
+	k_thread_name_set(idle_t,"Idle fake");	*/						   						   
 	mret_start = read_mret();
 	printf("start\n");
 	for(int i=0;i<NUM_LOOP;i++){
-		printf("loop %d\n",i);
+		//printf("loop %d\n",i);
 		end_loop = 1;
 		k_timer_start(&my_timer, K_MSEC(100), K_NO_WAIT);
 		while(end_loop){
-			printf("main sleeping\n");
+			//printf("main sleeping\n");
 			k_sleep(K_MSEC(50));
 
 		}
 	}
-	
+	printf("fine main loop\n");
 	end_t = 0;
 	k_thread_join(t1,K_FOREVER);
+	//k_thread_join(idle_t,K_FOREVER);
 	mret_end = read_mret();
 	printf("Stalli mret: %llu\n",mret_end-mret_start);
 	z_sys_poweroff();
